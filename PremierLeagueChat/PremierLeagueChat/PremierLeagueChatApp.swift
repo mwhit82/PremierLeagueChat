8f878dad868d9c4d9729c11217fb57f011932289
Mark Whitley <mark.whitley@mark.whitley>
2025-01-19 18:18:27 +0000

    Initial Xcode project setup

---
+++ PremierLeagueChat/PremierLeagueChat/PremierLeagueChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremierLeagueChatApp.swift
+//  PremierLeagueChat
+//
+//  Created by Mark Whitley on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PremierLeagueChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PremierLeagueChat/PremierLeagueChatTests/PremierLeagueChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  PremierLeagueChatTests.swift
+//  PremierLeagueChatTests
+//
+//  Created by Mark Whitley on 19/01/2025.
+//
+
+import Testing
+@testable import PremierLeagueChat
+
+struct PremierLeagueChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
